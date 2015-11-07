c31a8c97478e565e1c011c1d1f19f15fd08b94a0
Kien <autolame@yahoo.de>
2015-11-07 19:01:02 +0100

    S13 - added new scene for testing purposes

---
+++ GalleryPrototype/Assets/Scripts/InteractiveObjectScripts/InteractiveObject.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractiveObject : MonoBehaviour
+{
+    [SerializeField]
+    private bool _gotHit = false;
+
+    public bool GotHit
+    {
+        get
+        {
+            return _rigidBody.useGravity;
+        }
+        set
+        {
+            _gotHit = value;
+            _rigidBody.useGravity = _gotHit;
+            if (_gotHit == true)
+            {
+                _color = Color.red;
+            }
+            else
+            {
+                _color = Color.white;
+            }
+        }
+    }
+
+    private Rigidbody _rigidBody;
+    private Color _color;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        GotHit = false;
+        _color = Color.white;
+    }
+
+    private void OnMouseDown()
+    {
+        transform.SetParent(transform.parent.parent);
+
+        GotHit = !GotHit;
+        //_rigidBody.AddExplosionForce(1000f, new Vector3(0, 1f, 0), 500f);
+        _rigidBody.AddForce(transform.up * -1000);
+        GetComponent<Renderer>().material.color = _color;
+    }
+
+    public void OnInteraction()
+    {
+        OnMouseDown();
+    }
+}
---
+++ GalleryPrototype/Assets/Scripts/InteractiveObjectScripts/SphereHullObject.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereHullObject : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool _gotHit = false;
+
+    public bool GotHit
+    {
+        get
+        {
+            return _rigidBody.useGravity;
+        }
+        set
+        {
+            _gotHit = value;
+            _rigidBody.useGravity = _gotHit;
+            if (_gotHit == true)
+            {
+                _color = Color.red;
+            }
+            else
+            {
+                _color = Color.white;
+                ResetObject();
+            }
+        }
+    }
+
+    private Rigidbody _rigidBody;
+    private Color _color;
+
+    private Transform _defaultTransform;
+    private Transform _oldParent;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _defaultTransform = transform;
+        _oldParent = transform.parent;
+        Debug.Log(_defaultTransform);
+        ResetObject();
+    }
+
+    private void OnInteraction()
+    {
+        transform.SetParent(transform.parent.parent);
+        GotHit = !GotHit;
+        //_rigidBody.AddExplosionForce(1000f, new Vector3(0, 1f, 0), 500f);        
+
+
+        _rigidBody.AddForce(transform.up * -1000);
+        GetComponent<Renderer>().material.color = _color;
+    }
+    public void OnMouseDown()
+    {
+        OnInteraction();
+    }
+
+    public void ResetObject()
+    {
+        GotHit = false;
+        _color = Color.white;
+
+
+        Debug.Log("set transform default");
+
+        transform.SetParent(_defaultTransform.parent);
+        transform.localPosition = _defaultTransform.localPosition;
+        transform.localRotation = _defaultTransform.localRotation;
+    }
+
+}
