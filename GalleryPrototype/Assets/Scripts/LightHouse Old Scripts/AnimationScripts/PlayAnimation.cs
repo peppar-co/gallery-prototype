d0bda825e3e9575c9417d8a638d416f58f8804a4
Christoph Holtmann <christoph.holtmann@live.de>
2015-11-07 14:30:47 +0100

    R - Setup vuforia 5 plugin and some project settings

---
+++ GalleryPrototype/Assets/Scripts/LightHouse Old Scripts/AnimationScripts/PlayAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+
+    [SerializeField]
+    public int _animationState;
+
+    public void SetAnimationState(int state)
+    {
+        _animator.SetInteger("ID", state);
+    }
+
+
+}
