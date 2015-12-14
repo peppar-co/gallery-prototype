a4c62ab0bee125b880ef7f7e77282b4d3953516f
autolame <autolame@yahoo.de>
2015-12-15 00:06:57 +0100

    R - added reset showcase function reworked change expression reworked change scene repositioned GUI

--- GalleryPrototype/Assets/peppar/Scripts/Custom Components/EyeGUIController.cs
+++ GalleryPrototype/Assets/peppar/Scripts/Custom Components/EyeGUIController.cs
@@ -5,49 +5,91 @@ using UnityEngine.UI;
 
 namespace peppar
 {
-    public class EyeGUIController : MonoBehaviour
-    {
-        [SerializeField]
-        private EyeInteractionController _eyeInteractionController;
+	public class EyeGUIController : MonoBehaviour
+	{
+		[SerializeField]
+		private EyeInteractionController _eyeInteractionController;
 
-        [SerializeField]
-        private GameObject _buttonHolder;
+		[SerializeField]
+		private GameObject _buttonHolder;
 
-        [SerializeField]
-        private int _maxExpressions;
-        private void Awake()
-        {
-            UnityEngine.Assertions.Assert.IsNotNull(_eyeInteractionController);
+		[SerializeField]
+		private int _maxExpressions;
+		private void Awake()
+		{
+			UnityEngine.Assertions.Assert.IsNotNull(_eyeInteractionController);
 
-            _buttonHolder.SetActive(false);
-        }
-        public void OpenMenu()
-        {
-            _buttonHolder.SetActive(true);
-        }
-        public void CloseMenu()
-        {
-            _buttonHolder.SetActive(false);
-        }
+			_buttonHolder.SetActive(false);
+		}
+		public void OpenMenu()
+		{
+			_buttonHolder.SetActive(true);
+		}
+		public void CloseMenu()
+		{
+			_buttonHolder.SetActive(false);
+		}
 
-        public void ChangeExpresion()
-        {
-            int exp = _eyeInteractionController.EyeAnimator.GetInteger("Expression");
-            if (exp < _maxExpressions)
-            {
-                exp++;
-                _eyeInteractionController.EyeAnimator.SetInteger("Expression", exp);
-            }
-            else
-            {
-                exp = 0;
-                _eyeInteractionController.EyeAnimator.SetInteger("Expression", exp);
-            }
-        }
+		//public void ChangeExpresion()
+		//{
+		//	int exp = _eyeInteractionController.EyeAnimator.GetInteger("Expression");
+		//	if (exp < _maxExpressions)
+		//	{
+		//		exp++;
+		//		_eyeInteractionController.EyeAnimator.SetInteger("Expression", exp);
+		//	}
+		//	else
+		//	{
+		//		exp = 0;
+		//		_eyeInteractionController.EyeAnimator.SetInteger("Expression", exp);
+		//	}
+		//}
 
-        public void ChangeScene()
-        {
-            _eyeInteractionController.ChangeScene();
-        }
-    }
+		public void ResetExpression()
+		{
+			_eyeInteractionController.EyeAnimator.SetInteger("Expression", -1);
+		}
+
+		public void ResetScene()
+		{
+			_eyeInteractionController.ResetScene();
+		}
+
+		public void NextExpression()
+		{
+			int exp = _eyeInteractionController.EyeAnimator.GetInteger("Expression");
+			if (exp < _maxExpressions)
+			{
+				exp++;
+				_eyeInteractionController.EyeAnimator.SetInteger("Expression", exp);
+			}
+			else
+			{
+				exp = _maxExpressions;
+			}
+		}
+		public void PrevExpression()
+		{
+			int exp = _eyeInteractionController.EyeAnimator.GetInteger("Expression");
+			if (exp > 0)
+			{
+				exp--;
+				_eyeInteractionController.EyeAnimator.SetInteger("Expression", exp);
+			}
+			else
+			{
+				exp = 0;
+			}
+		}
+		public void NextScene()
+		{
+			_eyeInteractionController.ChangeScene(true);
+		}
+		public void PrevScene()
+		{
+			_eyeInteractionController.ChangeScene(false);
+		}
+
+
+	}
 }
--- GalleryPrototype/Assets/peppar/Scripts/Custom Components/EyeInteractionController.cs
+++ GalleryPrototype/Assets/peppar/Scripts/Custom Components/EyeInteractionController.cs
@@ -4,136 +4,165 @@ using System.Collections.Generic;
 
 namespace peppar
 {
-    public class EyeInteractionController : MonoBehaviour
-    {
-        [SerializeField]
-        private EyeGUIController _eyeGUIController;
+	public class EyeInteractionController : MonoBehaviour
+	{
+		[SerializeField]
+		private EyeGUIController _eyeGUIController;
 
-        [SerializeField]
-        private ObjectDropIn _objectDropIn;
+		[SerializeField]
+		private ObjectDropIn _objectDropIn;
 
-        [SerializeField]
-        private Animator _eyeAnimator;
+		[SerializeField]
+		private Animator _eyeAnimator;
 
-        [SerializeField]
-        private List<MeshRenderer> _stencilMeshes = new List<MeshRenderer>();
+		[SerializeField]
+		private List<MeshRenderer> _stencilMeshes = new List<MeshRenderer>();
 
-        [SerializeField]
-        private List<Material> _stencilMasks = new List<Material>();
+		[SerializeField]
+		private List<Material> _stencilMasks = new List<Material>();
 
-        private List<Material> _defaultStencilMasks = new List<Material>();
+		private List<Material> _defaultStencilMasks = new List<Material>();
 
-        private int _activeStencilIndex;
+		private int _activeStencilIndex;
 
-        public Animator EyeAnimator
-        {
-            get { return _eyeAnimator; }
-            set { _eyeAnimator = value; }
-        }
+		public Animator EyeAnimator
+		{
+			get { return _eyeAnimator; }
+			set { _eyeAnimator = value; }
+		}
 
-        [SerializeField]
-        private float _maxIdleTime;
+		[SerializeField]
+		private float _maxIdleTime;
 
-        private float _blinkTime;
+		private float _blinkTime;
 
-        public void ChangeScene()
-        {
-            if (_activeStencilIndex < _stencilMasks.Count - 1)
-            {
-                _activeStencilIndex++;
-                ChangeStencil(_activeStencilIndex);
-                _objectDropIn.Shuffle = true;
-            }
-            else
-            {
-                _activeStencilIndex = -1;
-                SetDefaultStencil();
-                _objectDropIn.Shuffle = true;
-            }
-        }
+		public void ChangeScene(bool next)
+		{
+			if (next == true)
+			{
+				_activeStencilIndex++;
+				if (_activeStencilIndex > _stencilMasks.Count - 1)
+				{
+					_activeStencilIndex = _stencilMasks.Count - 1;
+					return;
+				}
+				ChangeStencil(_activeStencilIndex);
+				_objectDropIn.Shuffle = true;
+			}
 
-        private void ChangeStencil(int index)
-        {
-            foreach (var mesh in _stencilMeshes)
-            {
-                mesh.material = _stencilMasks[index];
-            }
-        }
+			//if (_activeStencilIndex > _stencilMasks.Count - 1)
+			//{
 
-        private void SetDefaultStencil()
-        {
-            for (int i = 0; i < _stencilMeshes.Count; i++)
-            {
-                _stencilMeshes[i].material = _defaultStencilMasks[i];
-            }
-        }
+			//}
+			else
+			{
+				_activeStencilIndex--;
+				if (_activeStencilIndex < 0)
+				{
+					_activeStencilIndex = 0;
+					return;
+				}
+				ChangeStencil(_activeStencilIndex);
+				_objectDropIn.Shuffle = true;
+			}
+			//else
+			//{
+			//	_activeStencilIndex = 0;
+			//	SetDefaultStencil();
+			//	_objectDropIn.Shuffle = true;
+			//}
 
-        private void Blink()
-        {
-            _eyeAnimator.SetTrigger("Blink");
-        }
+		}
 
-        private void Awake()
-        {
-            UnityEngine.Assertions.Assert.IsNotNull(_eyeGUIController);
-            UnityEngine.Assertions.Assert.IsNotNull(_eyeAnimator);
-            UnityEngine.Assertions.Assert.IsNotNull(_objectDropIn);
-            UnityEngine.Assertions.Assert.IsNotNull(_stencilMeshes);
-            UnityEngine.Assertions.Assert.IsNotNull(_stencilMasks);
-            _maxIdleTime = 3;
-            _blinkTime = _maxIdleTime;
-            _activeStencilIndex = -1;
+		public void ResetScene()
+		{
+			_activeStencilIndex = 0;
+			SetDefaultStencil();
+			_objectDropIn.Shuffle = true;
+		}
 
-            if (_stencilMeshes != null)
-            {
-                foreach (var mesh in _stencilMeshes)
-                {
-                    _defaultStencilMasks.Add(mesh.material);
-                }
-            }
+		public void ChangeStencil(int index)
+		{
+			foreach (var mesh in _stencilMeshes)
+			{
+				mesh.material = _stencilMasks[index];
+			}
+		}
 
-        }
+		private void SetDefaultStencil()
+		{
+			for (int i = 0; i < _stencilMeshes.Count; i++)
+			{
+				_stencilMeshes[i].material = _defaultStencilMasks[i];
+			}
+		}
 
-        private void Update()
-        {
-            //toggle gui on hit
+		private void Blink()
+		{
+			_eyeAnimator.SetTrigger("Blink");
+		}
 
-            //get raycast from touch (android)       
-            for (var i = 0; i < Input.touchCount; ++i)
-            {
-                if (Input.GetTouch(i).phase == TouchPhase.Began)
-                {
-                    // Construct a ray from the current touch coordinates
-                    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+		private void Awake()
+		{
+			UnityEngine.Assertions.Assert.IsNotNull(_eyeGUIController);
+			UnityEngine.Assertions.Assert.IsNotNull(_eyeAnimator);
+			UnityEngine.Assertions.Assert.IsNotNull(_objectDropIn);
+			UnityEngine.Assertions.Assert.IsNotNull(_stencilMeshes);
+			UnityEngine.Assertions.Assert.IsNotNull(_stencilMasks);
+			_maxIdleTime = 3;
+			_blinkTime = _maxIdleTime;
+			_activeStencilIndex = -1;
 
-                    //utlDebugPrint.Inst.print(Input.GetTouch(i).position.ToString());
+			if (_stencilMeshes != null)
+			{
+				foreach (var mesh in _stencilMeshes)
+				{
+					_defaultStencilMasks.Add(mesh.material);
+				}
+			}
 
-                    if (Physics.Raycast(ray))
-                    {
-                        _eyeGUIController.OpenMenu();
-                    }
-                }
-            }
+		}
 
-            //mouse interaction for PC debugging
-            if (Input.GetMouseButtonDown(0))
-            {
-                Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		private void Update()
+		{
+			//toggle gui on hit
 
-                Debug.Log(Input.mousePosition);
+			//get raycast from touch (android)       
+			for (var i = 0; i < Input.touchCount; ++i)
+			{
+				if (Input.GetTouch(i).phase == TouchPhase.Began)
+				{
+					// Construct a ray from the current touch coordinates
+					Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
 
-                if (Physics.Raycast(mRay))
-                {
-                    _eyeGUIController.OpenMenu();
-                }
-            }
+					//utlDebugPrint.Inst.print(Input.GetTouch(i).position.ToString());
 
-            _maxIdleTime -= Time.deltaTime;
-            if (_maxIdleTime < 0)
-            {
-                _maxIdleTime = Random.RandomRange(.5f, _blinkTime);
-                Blink();
-            }
-        }
-    }
+					if (Physics.Raycast(ray))
+					{
+						_eyeGUIController.OpenMenu();
+					}
+				}
+			}
+
+			//mouse interaction for PC debugging
+			if (Input.GetMouseButtonDown(0))
+			{
+				Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+				//Debug.Log(Input.mousePosition);
+
+				if (Physics.Raycast(mRay))
+				{
+					_eyeGUIController.OpenMenu();
+				}
+			}
+
+			_maxIdleTime -= Time.deltaTime;
+			if (_maxIdleTime < 0)
+			{
+				_maxIdleTime = Random.RandomRange(.5f, _blinkTime);
+				Blink();
+			}
+		}
+	}
 }
