33b61a177c7c98e63ff0247788439e7abfc20808
Christoph Holtmann <christoph.holtmann@live.de>
2016-04-05 16:36:04 +0200

    R - finished basic GUI

---
+++ GalleryPrototype/Assets/peppar/Scripts/BIGW Scripts/Interface/BIGWGUIController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BIGWGUIController : MonoBehaviour
+{
+    public GameObject MenuCreateCharacter;
+    public GameObject MenuCreateCharacterName;
+    public GameObject MenuCreateFaceSnapshot;
+    public GameObject MenuIdle;
+
+    private void LateUpdate()
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+}
