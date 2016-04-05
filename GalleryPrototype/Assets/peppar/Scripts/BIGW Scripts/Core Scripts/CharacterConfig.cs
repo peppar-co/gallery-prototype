e5c9c5b83dd882b0dd32332fed189dec262cee12
Christoph Holtmann <christoph.holtmann@live.de>
2016-04-05 15:40:26 +0200

    R - added basic GUI added basic character dummy TODO: construct proper character class

---
+++ GalleryPrototype/Assets/peppar/Scripts/BIGW Scripts/Core Scripts/CharacterConfig.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterConfig : MonoBehaviour
+{
+    public List<GameObject> HairList = new List<GameObject>();
+    public List<GameObject> PantsList = new List<GameObject>();
+    public List<GameObject> ShirtList = new List<GameObject>();
+
+    class CharacterHair
+    {
+        public int Index;
+
+        public void Next()
+        {
+
+        }
+
+        public void Prev()
+        {
+
+        }
+    }
+
+    class CharacterShirt
+    {
+        public int Index;
+
+        public void Next()
+        {
+
+        }
+
+        public void Prev()
+        {
+
+        }
+    }
+
+    class CharacterPants
+    {
+        public int Index;
+
+        public void Next()
+        {
+
+        }
+
+        public void Prev()
+        {
+
+        }
+    }
+
+    class CharacterFace
+    {
+        public GameObject FaceGameObject;
+        Material FaceMaterial;
+
+        void AddFace(Texture2D faceTexture)
+        {
+            FaceMaterial.mainTexture = faceTexture;
+            FaceGameObject.GetComponent<Renderer>().material = FaceMaterial;
+        }
+
+
+    }
+}
