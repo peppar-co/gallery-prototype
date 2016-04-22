aa286305b48d8b80036b5907e1e1aea75d97131a
autolame <autolame@yahoo.de>
2016-04-22 18:11:32 +0200

    S - added pepp gui world canvas wired pepp gui

---
+++ GalleryPrototype/Assets/peppar/Scripts/BIGW Scripts/Interface/GUIQuestChoiceButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUIQuestChoiceButton : MonoBehaviour
+{
+    private string _buttonLabelText;
+    [SerializeField]
+    private Text _buttonLabel;
+
+    public string ButtonLabelText
+    {
+        get
+        {
+            return _buttonLabelText;
+        }
+
+        set
+        {
+            _buttonLabelText = value;
+            _buttonLabel.text = value;
+        }
+    }
+}
---
+++ GalleryPrototype/Assets/peppar/Scripts/BIGW Scripts/Interface/GUIQuestChoiceController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIQuestChoiceController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GUIQuestChoiceButton _choiceButtonA, _choiceButtonB;
+
+    public void SetChoiceButtonLabels(string textA, string textB)
+    {
+        _choiceButtonA.ButtonLabelText = textA;
+        _choiceButtonB.ButtonLabelText = textB;
+    }
+    
+
+
+}
