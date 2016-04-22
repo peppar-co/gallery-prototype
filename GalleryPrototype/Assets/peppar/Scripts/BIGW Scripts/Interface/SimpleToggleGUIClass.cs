bbd60880bc762cd97dcca361ef279ccc34a3261b
autolame <autolame@yahoo.de>
2016-04-22 17:35:32 +0200

    S - added simple toggle gui class

---
+++ GalleryPrototype/Assets/peppar/Scripts/BIGW Scripts/Interface/SimpleToggleGUIClass.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SimpleToggleGUIClass : MonoBehaviour
+{
+    [SerializeField]
+    private Text _referenceLabel;
+    [SerializeField]
+    private Toggle _referenceToggle;
+
+    private string _toggleText;
+    private bool _toggleActive;
+
+    public string ToggleText
+    {
+        get
+        {
+            return _toggleText;
+        }
+
+        set
+        {
+            _toggleText = value;
+            _referenceLabel.text = value;
+        }
+    }
+
+    public bool ToggleActive
+    {
+        get
+        {
+            return _toggleActive;
+        }
+
+        set
+        {
+            _toggleActive = value;
+            _referenceToggle.isOn = value;
+        }
+    }
+}
